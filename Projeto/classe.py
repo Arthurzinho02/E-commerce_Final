f7a419ae8ce8a3ebc0a465b6d515943e921504d4
Malaquia24 <arthur.matheus0607@gmail.com>
2023-09-21 08:54:49 -0300

    update

---
+++ Projeto/classe.py
@@ -0,0 +1,15 @@
+class Loja:
+    def __init__(self):
+        pass
+
+class Cliente:
+    def __init__(self):
+        pass
+
+class Produtos:
+    def __init__(self):
+        pass
+
+class ADM:
+    def __init__(self):
+        pass
